b83049da1d67a2e8b41f2114759a631d5345778b
Hugo Lindström <huggepugge1@gmail.com>
2022-09-03 17:08:28 +0200

    refactor

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,5 @@
-from board import print_board, compute_board, precompute_all_moves
+from board import print_board, compute_board
+from moves import precompute_all_moves
 
 white_pawn_moves = precompute_all_moves("white_pawn")
 black_pawn_moves = precompute_all_moves("black_pawn")
